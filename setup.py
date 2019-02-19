0841a074c69089cef6f2cf229a3426f40be50684
arunprasad <46923726+Arun1930@users.noreply.github.com>
2019-02-19 13:23:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
 
+from setuptools import setup
+
+setup(
+    name='rgbd',
+    version='0.1',
+    description='turn lights on',
+    url='https://github.com/Arun1930/rgb',
+    author='Arun prasad',
+    author_email='arun.prasad@xyzinnotech.com',
+    license='apache 2.0',
+    package_dir={'': 'src'},
+    packages=['rgb', 'rgb.modbus_module'],
+    entry_points={
+        'console_scripts': ['say-hello=datahub.modbus_module.say_hello:main'],
+    },
+    zip_safe=False)
